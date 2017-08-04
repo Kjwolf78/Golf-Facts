8dc75d3d033ab778e430029bfd29e119be16cd57
Kjwolf78 <kjwolf78@gmail.com>
2017-08-04 16:29:17 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ var Alexa = require('alexa-sdk');
 
 //Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.  
 //Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
-var APP_ID = "arn:aws:lambda:us-east-1:188921392272:function:ChampagneFact";
+var APP_ID = "arn:aws:lambda:us-east-1:188921392272:function:GolfFactSkill";
 
-var SKILL_NAME = "Champagne Facts";
+var SKILL_NAME = "Golf Facts";
 var GET_FACT_MESSAGE = "Here's your fact: ";
-var HELP_MESSAGE = "You can say tell me a Champagne fact, or, you can say exit... What can I help you with?";
+var HELP_MESSAGE = "You can say tell me a Golf fact, or, you can say exit... What can I help you with?";
 var HELP_REPROMPT = "What can I help you with?";
 var STOP_MESSAGE = "Goodbye!";
 
@@ -19,19 +19,19 @@ var STOP_MESSAGE = "Goodbye!";
 //TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
 //=========================================================================================================================================
 var data = [
-    "Marilyn Monroe bathed in Champagne which required 350 bottles to fill the tub.",
-    "There are approximately 49 million bubbles in one 750 milliliter bottle of Champagne.",
-    "Champagne has three times more gas than beer.",
-    "The pressure inside one Champagne bottle is three times that of an average car tire.",
-    "You are more likely to be killed by a flying Champagne cork than by a poisonous spider.",
-    "Dom Pierre Perignon was a nearly blind monk who is credited for the creation of Champagne.",
-    "More than a third of fatalities from Champagne corks occur at weddings.",
-    "The most expensive bottle of Champagne cost 2.07 million dollars.",
-    "It is usually less caloric than other alocholic beverages which makes it a prime selection of dieters.",
-    "It is rumored the coupe glass was molded from Marie Antoinette\’s left breast.",
-    "Sabrage is a term for opening a champagne bottle with an actual sword.",
-    "The longest recorded flight of a champagne cork was 177 feet.",
-    "There is currently 1 billion bottles of champagne in storage worldwide."
+    "The French word Cadet means youngest child and is where the word Caddy comes from.",
+    "A golf ball will travel further on hot days because the air is less dense so it takes less velocity to travel.",
+    "Golf balls used to be made from leather and chicken or goose feathers. One ball would cost ten to twenty dollars.",
+    "Only twenty percent of golfers have a handicap below 18. A golfer with handicap of 18 is called a Bogey Golfer.",
+    "Only two sports have ever been played while on the moon one is golf and the other is the javelin throw.",
+    "Until the invention and rising popularity of the golf tee golfers played off of sand piles they built themselves.",
+    "If you walked all eighteen holes instead of riding in a golf cart you would walk approximately four miles.",
+    "The chances of making two holes in one in a round of golf are one in 67 million.",
+    "The worlds first ever golf tournament for women was held on New Years Day in 1811 at Musselburgh.",
+    "Arnold Palmer won 95 Professional tournaments.",
+    "125,000 golf balls a year are hit into the water at the famous 17th hole of the Stadium Course at Sawgrass.",
+    "The longest putt ever is a monstrous 375 feet.",
+    "There are 336 dimples on a regulation golf ball."
 ];
 
 //=========================================================================================================================================
